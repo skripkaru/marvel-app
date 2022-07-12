a1ce690c21afe193daaa030b329ad4d581ec9175
Evgeniy Skripkaru <skripkaru.evgeniy@gmail.com>
2022-07-12 11:16:46 +0300

    add helmet

--- src/pages/comicsPage/ComicsPage.tsx
+++ src/pages/comicsPage/ComicsPage.tsx
@@ -1,3 +1,5 @@
+import { Helmet } from 'react-helmet'
+
 import AppBanner from '../../components/appBanner/AppBanner'
 import ErrorBoundary from '../../components/errorBoundary/ErrorBoundary'
 import ComicsList from '../../components/comicsList/ComicsList'
@@ -5,6 +7,10 @@ import ComicsList from '../../components/comicsList/ComicsList'
 const ComicsPage = () => {
   return (
     <>
+      <Helmet>
+        <meta name="description" content="Page with list of our comics" />
+        <title>Comics Page</title>
+      </Helmet>
       <AppBanner />
       <ErrorBoundary>
         <ComicsList />
